4ba6596044b29f4dbab6c04fda39ad70bdcf623f
dshramko <da.shramko@gmail.com>
2017-07-09 19:36:48 +0600

    Init. Add react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 8000,
+    host: "0.0.0.0",
+    publicPath: "/"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
